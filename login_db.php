a675f5e357c808942b9784b4326fd2a5bb6d5a3c
wichaisw <wichai.sw19@gmail.com>
2020-08-30 23:10:49 +0700

    complete php register system

---
+++ login_db.php
@@ -0,0 +1,34 @@
+<?php 
+  session_start();
+  include('server.php');
+  $errors = array();
+
+  if(isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    if(empty($username)) {
+      array_push($errors, "Username is required");
+    }
+
+    if(empty($password)) {
+      array_push($errors, "Password is required");
+    }
+
+    if(count($errors) == 0)  {
+      $password = md5($password);
+      $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password' ";
+      $result = mysqli_query($conn, $query);
+
+      if(mysqli_num_rows($result) == 1) {
+        $_SESSION['username'] = $username;
+        $_SESSION['success'] = "You are now logged in";
+        header("location: index.php");
+      } 
+    } else {
+      array_push($errors, "Wrong username/password combination");
+      $_SESSION['error'] = "Wrong username or password. Try again";
+      header("location: index.php");
+    }
+  }
+?>
